7facca97c9402673e82255e9d79a167b22e573b0
k3mbon <100402352+k3mbon@users.noreply.github.com>
2024-07-20 08:50:39 +0700

    fix login authentication

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,27 @@ import DocumentDetails from './components/DocumentDetails';
 import Agenda from './pages/Agenda';
 import Prestasi from './pages/Prestasi';
 import Carousel from './pages/Carousel';
+import { AuthProvider } from './auth/AuthProvider';
+import PrivateRoute from './auth/PrivateRoute';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/agenda" element={<Agenda />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/galeri" element={<Galeri />} />
-        <Route path="/prestasi" element={<Prestasi />} />
-        <Route path="/carousel" element={<Carousel />} />
-        <Route path="/artikel" element={<DocumentList />} />
-        <Route path="/document/:id" element={<DocumentDetails />} />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/agenda" element={<PrivateRoute><Agenda /></PrivateRoute>} />
+          <Route path="/posts" element={<PrivateRoute><Posts /></PrivateRoute>} />
+          <Route path="/galeri" element={<PrivateRoute><Galeri /></PrivateRoute>} />
+          <Route path="/prestasi" element={<PrivateRoute><Prestasi /></PrivateRoute>} />
+          <Route path="/carousel" element={<PrivateRoute><Carousel /></PrivateRoute>} />
+          <Route path="/artikel" element={<PrivateRoute><DocumentList /></PrivateRoute>} />
+          <Route path="/document/:id" element={<PrivateRoute><DocumentDetails /></PrivateRoute>} />
+          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,3 @@
-// src/components/Login.js
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
